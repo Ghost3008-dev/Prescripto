e05f867176be9dabe63df63b8fcf0b4b8e6b816c
Aryan <aryansaini3008@gmail.com>
2026-06-10 16:47:59 +0530

    Update contact details

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
             <li>+91 7703852300</li>
-            <li>shivamsofteng@gmail.com</li>
+            <li>prescripto.aryan@gmail.com</li>
           </ul>
         </div>
 
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className=' font-semibold text-lg text-gray-600'>OUR OFFICE</p>
           <p className=' text-gray-500'>Roshan Nagar <br />Faridabad ,Haryana</p>
-          <p className=' text-gray-500'>Tel: 9876543210 <br /> Email: shivamsofteng@gmail.com.com</p>
+          <p className=' text-gray-500'>Tel: 9876543210 <br /> Email: prescripto.aryan@gmail.com</p>
           <p className=' font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
